705d937bdba31dcc842e24732a564ef7d2a0bf10
mdb1710 <mdb1710@gmail.com>
2019-06-09 12:51:06 -0400

    added to context

--- src/config.js
+++ src/config.js
@@ -3,8 +3,8 @@
 // const REACT_APP_API_BASE_URL = 'https://ancient-retreat-93190.herokuapp.com/api'
 
 
-export default {
+module.exports = {
   BASE_API_URL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000/api',
-  
+  REACT_APP_API_BASE_URL: 'https://ancient-retreat-93190.herokuapp.com/api'
 }
 
